4ec4fdc2bf56563fa227db03ffba7a8c134a4f9f
Mirelle Mota <mirelle.motta06@gmail.com>
2022-12-26 16:04:52 -0300

    JavaScript

---
+++ 8_Boolean_op_logico/scripts.js
@@ -0,0 +1,7 @@
+console.log(5 > 3 && 3 == 2) /*true and false = false */
+console.log(5 > 3 || 3 == 1) /*true or false = true  */
+console.log(3 == 3 && 'Matheus' == 'Matheus') /*true and true = true */
+console.log("Felipe" == "João" || false) /*false or false = false */
+console.log(!true && true) /*true and true (not)= false */
+console.log(true && true) /*true and true = true */
+console.log(false || false) /*false or fale = false */
---
+++ 9_Boolean_op_ternario/scripts.js
@@ -0,0 +1,2 @@
+console.log(5 > 2 ? 'É sim' : 'É não')
+console.log('Mirelle' == 'Mirelle' ? 'Olá, Mirelle' : 'Não é a Mirelle')
